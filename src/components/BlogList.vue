4b1cbdab4b4f250a14c620e168d167b2f1b3f1aa
Anton de Regt <antonderegt@gmail.com>
2017-06-07 21:32:23 +0200

    Added social icons

--- src/components/BlogList.vue
+++ src/components/BlogList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Blogs</h1>
+    <h1>Blog</h1>
     <hr>
     <BlogPost title="How my site loads in 1.25s" link="https://medium.com/@antonderegt/how-my-site-loads-in-1-25s-56151a06ab8d" text="Last week I received a shocking message from Google. I forgot how it happened but Google asked me if I wanted to test how mobile friendly..."/>
     <BlogPost title="Keep Moving Your Fucking Fingers" link="https://medium.com/@antonderegt/https-medium-com-antonderegt-keep-moving-your-fucking-fingers-cdd1c631abf3" text="In this post, I’m going to talk about how a project I was trying to do with two dudes I never met before totally failed. First I’ll start..."/>
--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,18 +1,22 @@
 <template lang="html">
   <div>
-    <h2><a href="https://github.com/antonderegt" target="_blank" rel="noopener">Github</a></h2>
+    <h1>Social</h1>
+    <hr>
+    <h2><a href="https://github.com/antonderegt" target="_blank" rel="noopener"><img src="/assets/icons/github.svg" width="32px" alt="Github"></a></h2>
     <p>All of my code lives here.</p>
-    <h2><a href="https://www.youtube.com/c/AntondeRegt" target="_blank" rel="noopener">YouTube</a></h2>
+    <h2><a href="https://www.youtube.com/c/AntondeRegt" target="_blank" rel="noopener"><img src="/assets/icons/youtube.svg" width="32px" alt="YouTube"></a></h2>
     <p>I record my coding on YouTube.</p>
-    <h2><a href="https://medium.com/@antonderegt" target="_blank" rel="noopener">Medium</a></h2>
+    <h2><a href="https://medium.com/@antonderegt" target="_blank" rel="noopener"><img src="/assets/icons/medium.svg" width="32px" alt="Medium"></a></h2>
     <p>I write blogs on Medium.</p>
-    <h2><a href="https://soundcloud.com/antonderegt" target="_blank" rel="noopener">SoundCloud</a></h2>
+    <h2><a href="https://soundcloud.com/antonderegt" target="_blank" rel="noopener"><img src="/assets/icons/soundCloud.svg" width="32px" alt="SoundCloud"></a></h2>
     <p>I narate my blog posts on SoundCloud.</p>
-    <h2><a href="https://www.facebook.com/antonantonantonantonantonanton" target="_blank" rel="noopener">Facebook</a></h2>
+    <h2><a href="https://www.facebook.com/antonantonantonantonantonanton" target="_blank" rel="noopener"><img src="/assets/icons/facebook.svg" width="32px" alt="Facebook"></a></h2>
     Not a lot happends here anymore.
-    <h2><a href="https://twitter.com/antonderegt" target="_blank" rel="noopener">Twitter</a></h2>
+    <h2><a href="https://twitter.com/antonderegt" target="_blank" rel="noopener"><img src="/assets/icons/twitter.svg" width="32px" alt="Twitter"></a></h2>
     <p>A bot helps me with retweets.</p>
-    <h2><a href="https://www.linkedin.com/in/antonderegt/" target="_blank" rel="noopener">LinkedIn</a></h2>
+    <h2><a href="https://www.instagram.com/antonderegt/" target="_blank" rel="noopener"><img src="/assets/icons/instagram.svg" width="32px" alt="Instagram"></a></h2>
+    <p>All the pictures.</p>
+    <h2><a href="https://www.linkedin.com/in/antonderegt/" target="_blank" rel="noopener"><img src="/assets/icons/linkedIn.svg" width="32px" alt="LinkedIn"></a></h2>
     <p>The professional part.</p>
   </div>
 </template>
