9c61f8682cdb4a669e99f33e0b1859df6c50497e
antonderegt <antonderegt@gmail.com>
2017-09-26 21:21:28 +0200

    Added a resource

--- src/components/Resources.vue
+++ src/components/Resources.vue
@@ -3,6 +3,8 @@
     <div class="page-header">
       <h1>Resources</h1>
     </div>
+    <h3>React Native</h3>
+    <h4>Want to add remote push notifications to your RN app? <a href="https://medium.com/differential/react-native-push-notifications-with-onesignal-9db6a7d75e1e" target="_blank">Check out the blog post by Spencer Carli</a></h4>
     <h3>VueJS</h3>
     <h4>Awesome Vue course: <a href="https://www.udemy.com/vue-web-apps/learn/v4/overview" target="_blank">Vue JS - Mastering Web Apps</a></h4>
     <p>A lot of times you can find a discount for his courses on Udemy.</p>
